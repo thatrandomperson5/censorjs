05e6e46fbef6b36cd1b80da8bf9cd023e6658063
Nilts <94306853+thatrandomperson5@users.noreply.github.com>
2024-11-16 18:47:18 -0600

    Update censor.js

--- src/censor.js
+++ src/censor.js
@@ -155,6 +155,7 @@ class CensorObject {
    * @param {Object} object - The base object.
    */
   constructor(object) {
+    CensorObject.typeCheck(object, "object")
     this.object = object
     this.#eventHandleCache = {}
   }
@@ -284,7 +285,7 @@ class CensorObject {
 }
 
 /**
- * Practiaclly identical function-wise to {@link CensorObject}, used to censor uninitiated classes. The `genFunc` function listed below is the only unique function of this class.
+ * Practiaclly identical function-wise to {@link CensorObject}, used to censor uninitiated classes. The `genFunc` and `apply` functions listed below is the only unique functions of this class.
  * @class
  * @constructor
  * @public
@@ -300,6 +301,7 @@ class CensorClass {
    * @param {function(...*):Object} cls - The class descriptor function.
    */
   constructor(cls) {
+    CensorObject.typeCheck(cls, "function")
     this.cls = cls
     this.#eventHandles = {}
     this.#callHandles = {}
@@ -308,12 +310,36 @@ class CensorClass {
 
   whenCall(name, handle) {
     this.#callHandles[name] = handle
+    return this
   }
   whenAttr(name, handles) {
     this.#attrHandles[name] = handles
+    return this
   }
   on(name, handle) {
     this.#eventHandles[name] = handle
+    return this
+  }
+
+
+  /**
+   * Apply all censor handles to a instance of the given class. 
+   * @param {Object} obj - A instance of the given class or any other fitting object.
+   * @returns {CensorClass} - Returns self for chaining.
+   */
+  apply(obj) {
+    var c = new CensorObject(obj)
+
+    for (const [key, value] of Object.entries(this.#callHandles)) {
+      c.whenCall(key, value)
+    }
+    for (const [key, value] of Object.entries(this.#attrHandles)) {
+      c.whenAttr(key, value)
+    }
+    for (const [key, value] of Object.entries(this.#eventHandles)) {
+      c.on(key, value)
+    }
+    return this
   }
 
   /**
@@ -328,18 +354,7 @@ class CensorClass {
     let originalObject = this
     return function (...args) {
       var result = new originalObject.cls(...args)
-      var resultCensor = new CensorObject(result)
-
-      for (const [key, value] of Object.entries(originalObject.#callHandles)) {
-        resultCensor.whenCall(key, value)
-      }
-      for (const [key, value] of Object.entries(originalObject.#attrHandles)) {
-        resultCensor.whenAttr(key, value)
-      }
-      for (const [key, value] of Object.entries(originalObject.#eventHandles)) {
-        resultCensor.on(key, value)
-      }
-
+      originalObject.apply(result)
       return result
     }
   }
